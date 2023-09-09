eac172a12021ade4f8c3b34598eeef35ce8b2eff
ryangholland <rghcode@gmail.com>
2023-09-08 21:55:27 -0400

    Set up React and MaterialUI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+import CssBaseline from '@mui/material/CssBaseline';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+}
+
+export default App;
