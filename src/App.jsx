ee1510e9b95563a1df1c63845b66f5d027044b19
ryangholland <rghcode@gmail.com>
2023-09-08 23:15:40 -0400

    Style a To Do item

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import Checkbox from "@mui/material/Checkbox";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import FormControl from "@mui/material/FormControl";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -37,12 +40,31 @@ function App() {
                   fullWidth
                   sx={{ mb: 2 }}
                 />
-                <Button variant="contained" type="submit">Add Task</Button>
+                <Button variant="contained" type="submit">
+                  Add Task
+                </Button>
               </FormControl>
             </form>
           </Container>
         </Box>
         <hr></hr>
+        <Box
+          sx={{
+            pt: 2,
+            pb: 2,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Card variant="outlined" sx={{p:2}}>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Checkbox />
+                <Typography>
+                  This is a task. Yep. A task.
+                </Typography>
+              </Stack>
+            </Card>
+          </Container>
+        </Box>
       </main>
     </>
   );
